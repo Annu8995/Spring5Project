4ee2799b4f76f3e07a281dbcc1d5d05f66e831e9
Annu8995 <annuagarwal19951@gmail.com>
2021-07-03 11:51:00 +0530

    Spring Project Initial stage

---
+++ spring5project/src/test/java/annu/springframework/spring5project/Spring5projectApplicationTests.java
@@ -0,0 +1,13 @@
+package annu.springframework.spring5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
